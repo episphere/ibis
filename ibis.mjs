ebeb2ed1fee44455865008ad3e929ad1ee33694b
Jonas Almeida <jalmeida@mathbiol.org>
2023-08-30 15:46:09 -0400

    load test

--- ibis.mjs
+++ ibis.mjs
@@ -2,6 +2,23 @@ console.log(`ibis.mjs module loaded \n${Date()}`);
 
 const loaded=Date()
 
+async function getTestData(){
+    let txt = await (await fetch('https://proxy.jalmeida.workers.dev/?https://ems-trials.org/riskevaluator/software/Windows_x86/v6/example.txt')).text()
+    return txt.replace(/\\r/g,'')
+}
+
+async function txt2data(txt){
+    txt = txt? txt : await getTestData()
+    let dt = txt.split(/\\n/).map(r=>r.split(/\\t/))
+    dt.forEach(x=>{
+        4
+        
+    })
+    return dt
+}
+
 export {
-    loaded
+    loaded,
+    getTestData,
+    txt2data
 }
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ console.log(`index.js loaded\n${Date()}`);
 (async function(){
     ibis = await import('./ibis.mjs')
     //ibis = await import('https://episphere.github.io/ibis/ibis.mjs')
+    document.getElementById('getTestDataButton').onclick=async function(){
+        //document.getElementById('dataTextArea').value=await ibis.getTestData()
+        document.getElementById('dataTextArea').value=JSON.stringify(await ibis.txt2data())
+    }
     
 })()
 
